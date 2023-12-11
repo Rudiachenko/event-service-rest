58b84065ba02f4b9d8159c2d75044a3188df03e8
Fedir <fedir.rudiachenko@lseg.com>
2023-12-11 18:50:11 +0200

    config, db initializer completed; parent pom.xml extended

--- event-service-rest/src/main/resources/data.sql
+++ event-service-rest/src/main/resources/data.sql
@@ -1,9 +1,17 @@
-CREATE TABLE IF NOT EXISTS events (
-                                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                      title VARCHAR(255),
-                                      place VARCHAR(255),
-                                      speaker VARCHAR(255),
-                                      event_type VARCHAR(255),
-                                      date_time TIMESTAMP
+DROP TABLE IF EXISTS events;
+
+CREATE TABLE events
+(
+    id         bigserial    not null,
+    title      varchar(255) not null,
+    place      varchar(255) not null,
+    speaker    varchar(255) not null,
+    event_type varchar(255) not null,
+    date_time  timestamp    not null,
+    primary key (id)
 );
-INSERT INTO events (title, place, speaker, event_type, date_time) VALUES ('Tech Conference', 'New York', 'John Doe', 'Conference', '2023-12-05T09:00:00');
+
+INSERT INTO events(title, place, speaker, event_type, date_time)
+VALUES ('Two days concert', 'Kyiv, Ukraine', 'LATEXFAUNA', 'concert', '2023-12-22 19:00'),
+       ('NOTRE DAME DE PARIS Le Concert', 'Kyiv, Ukraine', 'Musical Band', 'concert', '2023-12-23 18:00'),
+       ('Underground StandUp', 'Kyiv, Ukraine', 'Hanna Kochegura', 'comedy', '2023-12-15 19:00');
