202fa13b31ddff1219aeb6eca71af1248c7c4e43
Fedir <fedir.rudiachenko@lseg.com>
2023-12-11 18:48:57 +0200

    Event-service-rest completed

---
+++ event-service-rest/src/main/java/com/epam/rest/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.epam.rest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.epam.impl")
+public class AppConfig {
+}
--- event-service-rest/src/main/java/com/epam/rest/controller/EventServiceController.java
+++ event-service-rest/src/main/java/com/epam/rest/controller/EventServiceController.java
@@ -1,14 +1,17 @@
 package com.epam.rest.controller;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import com.epam.api.service.EventService;
 import com.epam.dto.model.Event;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +19,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.epam.api.service.EventService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RestController
@@ -32,19 +35,34 @@ public class EventServiceController {
         this.eventService = eventService;
     }
 
+    @Operation(summary = "Create a new event", tags = {"Create Event"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created event"),
+            @ApiResponse(responseCode = "400", description = "Invalid event input")
+    })
     @PostMapping("/create")
     public ResponseEntity<Event> createEvent(@RequestBody Event event) {
         Event createdEvent = eventService.createEvent(event);
         return ResponseEntity.ok(createdEvent);
     }
 
-    @PutMapping("/{id}")
+    @Operation(summary = "Update an existing event", tags = {"Update Event"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated event"),
+            @ApiResponse(responseCode = "404", description = "Event not found")
+    })
+    @PutMapping("/update/{id}")
     public ResponseEntity<Event> updateEvent(@PathVariable Long id, @RequestBody Event event) {
         Event updatedEvent = eventService.updateEvent(id, event);
         return ResponseEntity.ok(updatedEvent);
     }
 
-    @GetMapping("/{id}")
+    @Operation(summary = "Get details of a specific event", tags = {"Get Event"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved event"),
+            @ApiResponse(responseCode = "404", description = "Event not found")
+    })
+    @GetMapping("/get/{id}")
     public EntityModel<Event> getEvent(@PathVariable Long id) {
         Event event = eventService.getEvent(id);
         return EntityModel.of(event,
@@ -56,20 +74,35 @@ public class EventServiceController {
                         .withRel("events"));
     }
 
-    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a specific event", tags = {"Delete Event"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successfully deleted event"),
+            @ApiResponse(responseCode = "404", description = "Event not found")
+    })
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteEvent(@PathVariable Long id) {
         eventService.deleteEvent(id);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @ApiOperation(value = "View a list of available events", response = List.class)
+    @Operation(summary = "Delete all event", tags = {"Delete Events"})
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully retrieved list"),
-            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+            @ApiResponse(responseCode = "204", description = "Successfully deleted event")
     })
-    @GetMapping
+    @DeleteMapping("/delete/all")
+    public ResponseEntity<?> deleteAllEvents() {
+        eventService.deleteAllEvents();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @Operation(summary = "View a list of available events", tags = {"Get all Events"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list"),
+            @ApiResponse(responseCode = "401", description = "You are not authorized to view the resource"),
+            @ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(responseCode = "404", description = "The resource you were trying to reach is not found")
+    })
+    @GetMapping("/get/all")
     public List<EntityModel<Event>> getAllEvents() {
         return eventService.getAllEvents().stream()
                 .map(event -> EntityModel.of(event,
@@ -80,7 +113,12 @@ public class EventServiceController {
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("/title/{title}")
+    @Operation(summary = "View a list of available events by title", tags = {"Get Events by Title"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list"),
+            @ApiResponse(responseCode = "404", description = "No events found with given title")
+    })
+    @GetMapping("/get/title/{title}")
     public List<EntityModel<Event>> getAllEventsByTitle(@PathVariable String title) {
         return eventService.getAllEventsByTitle(title).stream()
                 .map(event -> EntityModel.of(event,
@@ -89,5 +127,10 @@ public class EventServiceController {
                                         .getEvent(event.getId()))
                                 .withSelfRel()))
                 .collect(Collectors.toList());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
     }
 }
